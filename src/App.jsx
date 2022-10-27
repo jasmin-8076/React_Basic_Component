ef3d5f9dccdb2dcaaf55702a6ea4997a6555c380
jasmin-8076 <111449105+jasmin-8076@users.noreply.github.com>
2022-10-27 22:44:48 +0530

    hjgjfhgjgi

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Test from './Test'
+
+export default function App() {
+
+  return (
+   <div>
+    <h1>This is inside app component</h1>
+    <Test />
+    </div>
+    
+  )
+}
